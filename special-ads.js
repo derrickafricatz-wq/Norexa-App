6c08991d7ac514be64fe3dabf4b4042f45d90e43
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-04-29 23:17:38 +0300

    Remove lessonAds entry for key 1

--- special-ads.js
+++ special-ads.js
@@ -1,2 +1,1 @@
-const lessonAds={1:["https://i.imgur.com/P4qK4PP.jpeg"],
-                2:["https://i.imgur.com/lvRn9kT.jpeg","https://i.imgur.com/egaJf4s.png"]};
+const lessonAds={2:["https://i.imgur.com/lvRn9kT.jpeg","https://i.imgur.com/egaJf4s.png"],};
