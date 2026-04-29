aae217cb9f5d13dc5d890a36c1300bef0d54dc79
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-04-29 03:53:07 +0300

    Add files via upload

---
+++ special-ads.js
@@ -0,0 +1,2 @@
+const lessonAds={1:["https://i.imgur.com/UPgPdWT.jpeg","https://i.imgur.com/HdKedqJ.jpeg"],
+                2:["https://i.imgur.com/lvRn9kT.jpeg","https://i.imgur.com/egaJf4s.png"]};
