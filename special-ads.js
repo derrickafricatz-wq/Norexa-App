208a0ef8af23a4eafba57931b1697c9e0f9a7b12
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-04-29 23:05:47 +0300

    Update lessonAds to change image URLs

--- special-ads.js
+++ special-ads.js
@@ -1,2 +1,2 @@
-const lessonAds={1:["https://i.imgur.com/UPgPdWT.jpeg","https://i.imgur.com/HdKedqJ.jpeg"],
+const lessonAds={1:["https://i.imgur.com/P4qK4PP.jpeg"],
                 2:["https://i.imgur.com/lvRn9kT.jpeg","https://i.imgur.com/egaJf4s.png"]};
