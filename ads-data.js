1bc73452d51031ce46e1ba7a2f6286b55446b49e
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-18 12:26:50 +0300

    Update ads-data.js

--- ads-data.js
+++ ads-data.js
@@ -1,2 +1,2 @@
-const adImages=["https://i.imgur.com/037sQRn.png",
+const adImages=["https://derrickafricatz-wq.github.io/AFYA-CARE/images/ad3.png",
                 "https://i.imgur.com/X0bG0PE.png"];
