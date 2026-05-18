2159188a952a4b1fe5345e549a0743239e19f557
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-19 01:52:58 +0300

    Update adImages array with new image URL

--- ads-data.js
+++ ads-data.js
@@ -1,2 +1,2 @@
 const adImages=["https://derrickafricatz-wq.github.io/AFYA-CARE/images/ad3.png",
-                "https://i.imgur.com/X0bG0PE.png"];
+                "https://derrickafricatz-wq.github.io/AFYA-CARE/images/one.png"];
