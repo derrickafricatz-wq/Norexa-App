f2d64dd924a872d23aefdcf1496f703c0f86827e
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-19 02:48:56 +0300

    Update ad image URL from PNG to JPG format

--- ads-data.js
+++ ads-data.js
@@ -1,2 +1,2 @@
 const adImages=["https://derrickafricatz-wq.github.io/AFYA-CARE/images/ad3.png",
-                "https://derrickafricatz-wq.github.io/AFYA-CARE/images/one.png"];
+                "https://derrickafricatz-wq.github.io/AFYA-CARE/images/one.jpg"];
