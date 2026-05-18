3544bc262a3a3844764019841ea5e7b491d725d7
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-18 12:41:59 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,47 @@
-self.addEventListener("install", () => {
-  console.log("App installed");
+const CACHE_NAME = "afya-care-v1";
+
+const urlsToCache = [
+  "/AFYA-CARE/",
+  "/AFYA-CARE/index.html",
+
+  // VIDEOS
+  "/AFYA-CARE/videos/wow.mp4",
+
+  // IMAGES
+  "/AFYA-CARE/images/ad3.png"
+];
+
+self.addEventListener("install", (event) => {
+
+  event.waitUntil(
+
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Caching files...");
+        return cache.addAll(urlsToCache);
+      })
+
+  );
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then((response) => {
+
+        // Return cached version if found
+        if(response){
+          return response;
+        }
+
+        // Otherwise fetch from internet
+        return fetch(event.request);
+
+      })
+
+  );
+
 });
