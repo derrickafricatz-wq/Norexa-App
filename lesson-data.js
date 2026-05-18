6dad17e1315322ae7e0be053ff55d9cc57ce8cc4
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-19 01:25:15 +0300

    Fix formatting of lesson-data.js

--- lesson-data.js
+++ lesson-data.js
@@ -1,3 +1,3 @@
 let lessons={
 1:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/wow.mp4"],
-2:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4∆"]};
+2:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4"]};
