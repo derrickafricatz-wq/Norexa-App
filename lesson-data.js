adcd74cff4ecdf88ec6d507d104814cdcec19789
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-19 01:24:26 +0300

    Update lesson links in lesson-data.js

--- lesson-data.js
+++ lesson-data.js
@@ -1,5 +1,3 @@
 let lessons={
 1:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/wow.mp4"],
-2:["https://i.imgur.com/nXmK9G4.mp4","https://i.imgur.com/AgYGVSb.mp4"],
-3:["https://i.imgur.com/Hb4qooS.mp4"],
-10:["https://i.imgur.com/UEYYSmp.mp4"]};
+2:["https://derrickafricatz-wq.github.io/AFYA-CARE/videos/usafi.mp4∆"]};
